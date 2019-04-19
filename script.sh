0f956d26b9200c2a66e78ed098aa24137c4be256
Jenkins Automation Server <jenkins@localhost.localdomain>
2019-04-19 07:51:46 -0700

    ajout de la version dans le nom du jar

--- script.sh
+++ script.sh
@@ -15,18 +15,18 @@ then
 	NBVERSION=$(cat inside_metadata.xml | grep "<value>" | tail -1 | tr -d "<value> " | tr -d "/")
 	echo $NBVERSION
 	JAR=$(echo $LINK/$VERSION/server-$NBVERSION.jar)
-	wget -O server.jar $JAR
-	scp -r -p ./server.jar user01@192.168.100.89:/data/projet/
+	wget -O server"$VERSION".jar $JAR
+	scp -r -p ./server"$VERSION".jar user01@192.168.100.89:/data/projet/
 else
 	echo "version release detectée"
 	LINK=http://192.168.100.79:8081/repository/maven-releases/com/lesformateurs/maven-project/server
         echo $VERSION
         JAR=$(echo $LINK/$VERSION/server-$VERSION.jar)
-	wget -O server.jar $JAR
+	wget -O server"$VERSION".jar $JAR
 	if [ $? -ne 0 ]
 	then
 		echo "le numéro de version n'existe pas sur le serveur"
 		exit 2
 	fi
-        scp -r -p ./server.jar user01@192.168.100.89:/data/projet/
+        scp -r -p ./server"$VERSION".jar user01@192.168.100.89:/data/projet/
 fi
