d100a6b5d93038ef13a4474417cc01042bf7a4c8
Jenkins Automation Server <jenkins@localhost.localdomain>
2019-04-19 07:47:47 -0700

    ajout du scp dans le script.sh

--- script.sh
+++ script.sh
@@ -15,16 +15,18 @@ then
 	NBVERSION=$(cat inside_metadata.xml | grep "<value>" | tail -1 | tr -d "<value> " | tr -d "/")
 	echo $NBVERSION
 	JAR=$(echo $LINK/$VERSION/server-$NBVERSION.jar)
-	wget $JAR
+	wget -O server.jar $JAR
+	scp -r -p ./server.jar user01@192.168.100.89:/data/projet/
 else
 	echo "version release detectée"
 	LINK=http://192.168.100.79:8081/repository/maven-releases/com/lesformateurs/maven-project/server
         echo $VERSION
         JAR=$(echo $LINK/$VERSION/server-$VERSION.jar)
-	wget $JAR
+	wget -O server.jar $JAR
 	if [ $? -ne 0 ]
 	then
 		echo "le numéro de version n'existe pas sur le serveur"
 		exit 2
 	fi
+        scp -r -p ./server.jar user01@192.168.100.89:/data/projet/
 fi
