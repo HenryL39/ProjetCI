73b5b941242f5d2807210bc6f25eac1daa4f08bd
Jenkins Automation Server <jenkins@localhost.localdomain>
2019-04-19 07:37:53 -0700

    Changement du script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,3 @@
-#LINK=http://192.168.100.79:8081/repository/maven-snapshots/com/lesformateurs/maven-project/server
-#wget $LINK/maven-metadata.xml
-#VERSION=$(cat maven-metadata.xml | grep "<version>" | tail -1 | tr -d '<version> ' | tr -d '/')
 VERSION=$1
 SNAPSHOT="SNAPSHOT"
 echo $VERSION
@@ -14,22 +11,15 @@ then
 		echo "numéro de version n'existe pas sur le serveur"
 		exit 1
 	fi
-	#LATEST=$(cat maven-metadata.xml | grep "<lastUpdated>" | tr -d '<lastUpdated> ' | tr -d '/')
 	echo $VERSION
 	NBVERSION=$(cat inside_metadata.xml | grep "<value>" | tail -1 | tr -d "<value> " | tr -d "/")
 	echo $NBVERSION
-	#echo $LATEST
 	JAR=$(echo $LINK/$VERSION/server-$NBVERSION.jar)
 	wget $JAR
 else
-#then
 	echo "version release detectée"
 	LINK=http://192.168.100.79:8081/repository/maven-releases/com/lesformateurs/maven-project/server
-        #LATEST=$(cat maven-metadata.xml | grep "<lastUpdated>" | tr -d '<lastUpdated> ' | tr -d '/')
         echo $VERSION
-        #NBVERSION=$(cat inside_metadata.xml | grep "<value>" | tail -1 | tr -d "<value> " | tr -d "/")
-        #echo $NBVERSION
-        #echo $LATEST
         JAR=$(echo $LINK/$VERSION/server-$VERSION.jar)
 	wget $JAR
 	if [ $? -ne 0 ]
@@ -37,7 +27,4 @@ else
 		echo "le numéro de version n'existe pas sur le serveur"
 		exit 2
 	fi
-#else
-#	echo "veuillez rentrer un numéro de version ou de snapshot valide"
-#	exit 3
 fi
